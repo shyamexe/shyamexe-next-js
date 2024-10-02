78abe27fbfd5cf8adfc780e3f5c838f69dbceaa3
shyam <shyamjith38@hotmail.com>
2024-10-02 17:09:05 +0530

    import fixes ✈️

--- src/components/about/BlogTime.tsx
+++ src/components/about/BlogTime.tsx
@@ -1,5 +1,4 @@
-import React from 'react'; 
-import Image from 'next/image';
+import React from 'react';  
 interface BlogTileProps {
   date: string;
   title: string;
--- src/components/project_card.tsx
+++ src/components/project_card.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useRef } from 'react';
+import {  useRef } from 'react';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from '@gsap/react';
